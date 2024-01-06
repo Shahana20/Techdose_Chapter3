003fd42e34f95790bc329ce81a825e7478b9be4a
Shahana20 <107378734+Shahana20@users.noreply.github.com>
2024-01-06 15:48:04 +0530

    Time: 5 ms (45.66%) | Memory: 45.2 MB (13.99%) - LeetSync

--- 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.java
+++ 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.java
@@ -2,45 +2,26 @@ class Solution {
     public boolean isVowel(char ch)
     {
         ch = Character.toLowerCase(ch);
-        return ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u';
+        return ch == 'a' || ch == 'e' || ch == 'i' || ch =='o' || ch == 'u';
     }
-    
-    public void swap(char[] arr , int index1 , int index2)
-    {
-        char temp = arr[index1];
-        arr[index1] = arr[index2];
-        arr[index2] = temp;
-    }
-
     public String reverseVowels(String s) {
-        char[] arr = s.toCharArray();
-        int left = 0;
-        int right = s.length() - 1;
-        while(left < right)
+        char[] str = s.toCharArray();
+        List<Character> vowel = new ArrayList<>();
+
+        for(char ch : str)
         {
-            // System.out.println(s.charAt(left)+" "+s.charAt(right));
-            if(isVowel(arr[left]) && isVowel(arr[right]))
-            {
-                swap(arr , left , right);
-                // System.out.println(s.charAt(left)+" "+s.charAt(right));
-                left++;
-                right--;
-            }
-            else if(isVowel(arr[left]))
-            {
-                right--;
-            }
-            else if(isVowel(arr[right]))
-            {
-                left++;
-            }
-            else
-            {
-                left++;
-                right--;
-            }
-            
+            if(isVowel(ch)) vowel.add(ch);
         }
-        return new String(arr);
+
+        int index = vowel.size() - 1;
+        for(int i = 0 ; i < str.length ; i++)
+        {
+            if(isVowel(str[i]))
+            {
+                str[i] = vowel.get(index);
+                index--;
+            }
+        }
+        return new String(str);
     }
 }
